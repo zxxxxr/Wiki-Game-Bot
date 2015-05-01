bdb7148b19b6a85faec6e0401f196b533e52b932
Xiuruo Zhang <xiuruo@seas>
2015-05-01 13:21:38 -0400

    Make labled edge ByteString

--- Graphs.hs
+++ Graphs.hs
@@ -4,31 +4,37 @@
 module Graphs where
 
 import Data.ByteString (ByteString)
+import Data.ByteString as BS (append)
 import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.NodeMap
 import Data.Graph.Inductive.PatriciaTree
 import Data.Graph.Inductive.Query.BFS
 
 
-buildLEdgeList :: [(ByteString, [ByteString])] -> [(ByteString, ByteString, ())]
+getLEgStr :: ByteString -> ByteString -> ByteString
+getLEgStr from = BS.append from . BS.append " -> "
+
+
+buildLEdgeList :: [(ByteString, [ByteString])]
+                -> [(ByteString, ByteString, ByteString)]
 buildLEdgeList pages = concatMap makeEdges pages
     where alltitles = fst $ unzip pages :: [ByteString]
-          makeEdges (title, links) = map (\l -> (title, l, ())) $
+          makeEdges (title, links) = map (\l -> (title, l, getLEgStr title l)) $
                                      filter (`elem` alltitles) links
 
 
 buildGraph :: [(ByteString, [ByteString])]
-                -> (Gr ByteString (), NodeMap ByteString)
+                -> (Gr ByteString ByteString, NodeMap ByteString)
 buildGraph pages = mkMapGraph (fst $ unzip pages)
                           $ buildLEdgeList pages
 
 getShortestPath :: ByteString
                     -> ByteString
-                    -> (Gr ByteString (), NodeMap ByteString)
+                    -> (Gr ByteString ByteString, NodeMap ByteString)
                     -> Maybe [ByteString]
 getShortestPath from to g = case path of
-    [] -> Nothing
-    ls -> sequence $ map (lab $ fst g) ls
-    where path = esp fromN toN $ fst g
+    LP [] -> Nothing
+    LP ls -> Just $ tail $ map snd ls -- sequence $ map (lab $ fst g) ls
+    where path = lesp fromN toN $ fst g :: LPath ByteString
           fromN = fst $ mkNode_ (snd g) from
           toN   = fst $ mkNode_ (snd g) to
